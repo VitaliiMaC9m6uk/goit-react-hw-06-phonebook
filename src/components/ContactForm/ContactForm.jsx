727bad049d529414eb0a391e12304f40d7748307
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-06-12 22:36:34 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,13 +4,37 @@ const ContactForm = ({submit}) => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
+  const isValidLogin = (value) => {
+    const pattern = /^[a-zA-Zа-яА-ЯЁё ]+$/;      
+    return pattern.test(value);
+  }
+  const isValidNumber = value => {
+    const pattern =
+      /[0-9]{12}/;    
+    return pattern.test(value);
+  };
+
   const hendleChange = ({ target: { value, name } }) => {     
     switch (name) {
-      case 'name':
-        setName(value)
+      case 'name':  
+        if (value === '') {
+         setName(value); 
+        }
+        if (!isValidLogin(value)) {
+          
+
+          return;
+        }
+        setName(value);
         break;
       case 'number':
-        setNumber(value)
+        if (value === '') {
+          setNumber(value);
+        }
+        if (!isValidNumber(value)) {          
+          setNumber(value);
+          return;
+        }       
         break;
       default:
         return;
@@ -30,8 +54,7 @@ const ContactForm = ({submit}) => {
       <input
         onChange={hendleChange}
         type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        name="name"       
         value={name}
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
@@ -41,8 +64,7 @@ const ContactForm = ({submit}) => {
         type="tel"
         value={number}
         onChange={hendleChange}
-        name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        name="number"       
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       ></input>
